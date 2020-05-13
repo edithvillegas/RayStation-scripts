3249ec345be74eecca4cdfe904ea2003373957b5
edithvillegas <edith.natalia.villegas.garcia@cern.ch>
2020-05-14 01:29:27 +0200

    make selected roi list, add roi algebra

--- overlapping_structures.py
+++ overlapping_structures.py
@@ -26,7 +26,7 @@ external_name = 'External_ICTP'
 exclusion_list = [ptv_name, ctv_name, external_name]
 
 #create empty dictionary to store results
-distances = {}
+roi_distances = {}
 
 #loop over all rois to calculate distance to PTV
 for roi in rois:
@@ -35,8 +35,70 @@ for roi in rois:
     else:
         distance = structures.RoiSurfaceToSurfaceDistanceBasedOnDT(
                 ReferenceRoiName = ptv_name, TargetRoiName = roi.Name)
-        distances[roi.Name] = distance['Min']
+        roi_distances[roi.Name] = distance['Min'] #save just the min distance
         
-print distances
+#select the ROIs that are within a certain distance of the PTV
+margin = 0.5 #cm
+
+selected_rois = []
+
+for roi in roi_distances:
+    if roi_distances[roi]<margin :
+        selected_rois.append(roi)
+
+#Check that selected rois are organs
+for roi in selected_rois:
+    pass
+
+#------------------------------------------------------------------------------
+#Create boolean ROIs for the selected organs      
+
+#Create an empty ROI
+ename = 'empty'
+ecolor = '#FFFF00FF' #ARGB value
+etype = 'Control'
+#etissuename = None
+#erbecelltype = None
+#eroimaterial = None 
+#
+#boolean_roi = case.PatientModel.CreateRoi(
+#        Name=ename, Color=ecolor, Type=etype,
+#        TissueName=etissuename, RbeCellTypeName=erbecelltype,
+#        RoiMaterial=eroimaterial)
+
+boolean_roi = case.PatientModel.CreateRoi(Name=ename, Color=ecolor, Type=etype)
+
+#now create an algebra roi from this
+#margin settings
+margin_expand = {'Type': "Expand", 'Superior': 0.5, 'Inferior': 0.5, 
+                 'Anterior': 0.5, 'Posterior': 0.5, 'Right': 0.5, 'Left': 0.5}
+margin_same = {'Type': "Expand", 'Superior': 0, 'Inferior': 0, 
+                 'Anterior': 0, 'Posterior': 0, 'Right': 0, 'Left': 0}
+
+expressiona = {'Operation': "Union", 'SourceRoiNames': [r"Bladder_ICTP"], 
+               'MarginSettings': margin_same }
     
+expressionb = {'Operation': "Union", 'SourceRoiNames': [r"PTV_ICTP"], 
+               'MarginSettings': margin_expand}
+
+#boolean_roi.CreateAlgebraGeometry(
+#    Examination=examination, Algorithm='Auto', 
+#    )
+
+
+###copied from script
+with CompositeAction('ROI Algebra (empty)'):
+
+  expression = boolean_roi.SetAlgebraExpression(ExpressionA=expressiona, 
+                                                ExpressionB=expressionb, 
+                                                ResultOperation="Subtraction", 
+                                                ResultMarginSettings=margin_same)
+
+  expression.UpdateDerivedGeometry(Examination=examination, Algorithm="Auto")
+
+  # CompositeAction ends 
+
+
+
+
 
