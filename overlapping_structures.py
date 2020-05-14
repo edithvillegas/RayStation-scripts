b400d22c1a2eaa8dc87c7fdd5f2c96e2e987bac6
edithvillegas <edith.natalia.villegas.garcia@cern.ch>
2020-05-14 02:26:56 +0200

    added loop to create all control structures

--- overlapping_structures.py
+++ overlapping_structures.py
@@ -3,10 +3,22 @@
 IronPython 2.7
 Created on Wed May 13 21:27:33 2020
 @author: EV
+
+1.Checks which ROIs overlap with the PTV (plus a margin),
+2.Creates control structures that dont overlap with the PTV, for each of the
+overlapping ROIs.
 """
 
 from connect import *
 import sys
+
+#ROIs to exclude from the comparisons
+ptv_name = 'PTV_ICTP'
+ctv_name = 'CTV_ICTP'
+external_name = 'External_ICTP'
+exclusion_list = [ptv_name, ctv_name, external_name]
+#margin (maximum distance to PTV)  
+margin = 0.5 #cm
 
 #get current case and examination
 case = get_current("Case")
@@ -17,14 +29,7 @@ structures = case.PatientModel.StructureSets[examination.Name]
 #ROIs in the current case
 rois = case.PatientModel.RegionsOfInterest 
 
-#ROI names
-ptv_name = 'PTV_ICTP'
-ctv_name = 'CTV_ICTP'
-external_name = 'External_ICTP'
-
-#ROIs to exclude from the comparisons
-exclusion_list = [ptv_name, ctv_name, external_name]
-
+#1------------------------------------------------------------------------------
 #create empty dictionary to store results
 roi_distances = {}
 
@@ -38,8 +43,6 @@ for roi in rois:
         roi_distances[roi.Name] = distance['Min'] #save just the min distance
         
 #select the ROIs that are within a certain distance of the PTV
-margin = 0.5 #cm
-
 selected_rois = []
 
 for roi in roi_distances:
@@ -50,40 +53,44 @@ for roi in roi_distances:
 for roi in selected_rois:
     pass
 
-#------------------------------------------------------------------------------
-#Create boolean ROIs for the selected organs      
-original_name = r"Bladder_ICTP"
-ptv_name = 'PTV_ICTP'
+#2------------------------------------------------------------------------------
+#Create boolean ROIs for the selected organs  
 
-#Create an empty ROI to start
 #ROI parameters
-ename = original_name+ "_mPTV"
-ecolor = '#FFFF00FF' #ARGB value, purple
-etype = 'Control'
-#create ROI
-boolean_roi = case.PatientModel.CreateRoi(Name=ename, Color=ecolor, Type=etype)
+purple = '#FFFF00FF' #ARGB value, purple
 
-#Create ROI algebra to fill the empty ROI
+#Algebra settings
 #margin settings
 margin_expand = {'Type': "Expand", 'Superior': margin, 'Inferior': margin, 
                  'Anterior': margin, 'Posterior': margin, 'Right': margin, 
                  'Left': margin}
 margin_same = {'Type': "Expand", 'Superior': 0, 'Inferior': 0, 
                  'Anterior': 0, 'Posterior': 0, 'Right': 0, 'Left': 0}
-#expressions (organ and PTV+margin)
-expressiona = {'Operation': "Union", 'SourceRoiNames': [original_name], 
-               'MarginSettings': margin_same }
-expressionb = {'Operation': "Union", 'SourceRoiNames': [ptv_name], 
+#expression PTV+margin
+expression_ptv = {'Operation': "Union", 'SourceRoiNames': [ptv_name], 
                'MarginSettings': margin_expand}
 
-#do ROI algebra 
-with CompositeAction('ROI Algebra'):
-  expression = boolean_roi.SetAlgebraExpression(ExpressionA=expressiona, 
-                                                ExpressionB=expressionb, 
+
+#Loop over all selected organs, create empty ROI and fill it 
+for organ_roi in selected_rois:
+    algebra_name = organ_roi + "_mPTV"
+
+    #create empty ROI
+    with CompositeAction('Create ROI'):
+        boolean_roi = case.PatientModel.CreateRoi(Name=algebra_name, 
+                                              Color=purple, Type='Control')
+    #end composite action
+
+    #expressions (organ expression)
+    expression_organ = {'Operation': "Union", 'SourceRoiNames': [organ_roi], 
+               'MarginSettings': margin_same }
+
+    #do ROI algebra to fill in ROI
+    with CompositeAction('ROI Algebra'):
+        expression = boolean_roi.SetAlgebraExpression(ExpressionA=expression_organ, 
+                                                ExpressionB=expression_ptv, 
                                                 ResultOperation="Subtraction", 
                                                 ResultMarginSettings=margin_same)
 
-  expression.UpdateDerivedGeometry(Examination=examination, Algorithm="Auto")
-  # CompositeAction ends 
-
-
+        expression.UpdateDerivedGeometry(Examination=examination, Algorithm="Auto")
+    # CompositeAction ends 
